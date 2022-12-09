69e0faef114bd1cdecaf31b445f73b9c288a10be
mirrortype <ellamong7@gmail.com>
2022-12-09 18:57:47 +0900

    ㅡㅑㅓㅕㅐㅑㅓㅗㅕㅐㅓㅐㅑ

---
+++ pythonbasic/exam01.py
@@ -0,0 +1,11 @@
+who = ["명석", "지현", "서윤", "홍석"]
+where = ["화장실", "교실", "운동장", "편의점"]
+what = ["양말", "지우개", "샤프심", "교통카드"]
+
+import random
+
+random.choice(who)
+random.choice(where)
+random.choice(what)
+
+print(f"{murderer}이가 {ocasion}에서 {tool}"(으)로 사람을 죽였다.")
